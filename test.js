53a461aa05485873447e8c178e7309f3e99d2267
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-02-02 11:44:13 -0600

    Updating test.js to be similar to other database-js-* drivers

--- test.js
+++ test.js
@@ -8,26 +8,26 @@ var Connection = Postgres.open({
     Database: 'test'
 });
 
-var promises = [];
-promises.push(Connection.execute('DROP TABLE IF EXISTS test1; CREATE TABLE test1 (key character varying(255), value character varying(1024));').then(() => {
-    promises.push(Connection.execute("INSERT INTO test1 VALUES('name', 'Michael Anderson');").then(() => {
-        promises.push(Connection.query("SELECT * FROM test1 WHERE key = 'name';").then(data => {
-            if (data.length != 1) {
-                console.error('Invalid data returned');
-                Connection.execute('DROP TABLE test1;').then(() => {
-                    Connection.close().then(() => {
-                        process.exit(1);
-                    });
-                })
-            }
-        }));
-    }));
-}));
+function handleError(error) {
+    console.log("ERROR:", error);
+    process.exit(1);
+}
 
-Promise.all(promises).then(() => {
-    Connection.execute('DROP TABLE IF EXISTS test1;').then(() => {
-        Connection.close().then(() => {
-            process.exit(0);
-        });
-    });
-});
+Connection.execute('DROP TABLE IF EXISTS test1;').then(() => {
+    Connection.execute('CREATE TABLE test1 (fl_name varchar(32), fl_value varchar(32));').then(() => {
+        Connection.execute("INSERT INTO test1 VALUES('name', 'Michael Anderson');").then(() => {
+            Connection.query("SELECT * FROM test1 WHERE fl_name = 'name';").then(data => {
+                if (data.length != 1) {
+                    return handleError(new Error("Invalid data returned"));
+                }
+                console.log(data);
+                Connection.execute('DROP TABLE IF EXISTS test1;').then(() => {
+                    Connection.close().then(() => {
+                        process.exit(0);
+                    }).catch(handleError);
+                }).catch(handleError);
+            }).catch(handleError);
+        }).catch(handleError);
+    }).catch(handleError);
+}).catch(handleError);
+
